ced895257fba2fce653b165b124affd52fdff418
Jason Tan <ac113173@avcol.school.nz>
2022-03-11 21:23:55 +1300

    Added Start Command, Randomly Selected Words For Quiz, 1st Question, -1 From Score If Incorrect Guess

--- Te Reo Maori Quiz Assessment/Program.cs
+++ Te Reo Maori Quiz Assessment/Program.cs
@@ -1,5 +1,8 @@
 ﻿
+int score = 60;
 
+// Member, Article, Achieve, Conclusion, Bottom, Ally, Spray, Carry, Jail, Shoot, Galaxy, Compact, Highway, Sacred, Tool
+string[] words = new string[] {"mema", "atikara", "tutuki", "mutunga", "whakatakere", "haumi", "wairehu", "pakihere", "whare herehere", "pipihi", "ikarangi", "kiato", "huanui", "puaroa", "keteparaha"};
 
 Console.WriteLine("This Is A '15 Level' Quiz To Increase Or Train Your Knowledge On The Te Reo Maori Language");
 Console.WriteLine("You Are Going To Be Given One Word In Maori And You Are To Type From 4 Words Given That Are Translated In English");
@@ -14,3 +17,22 @@ while (start != "start")
     start = Console.ReadLine();
 }
 
+if (start == "start")
+{
+    Console.WriteLine($"Your First Word Is... {words[0]}");
+    Console.WriteLine("A. Dilemma \nB. Memory \nC. Remember \nD. Member \n(Remember To Type The Word)");
+    string guess = Console.ReadLine();
+    while (guess == "Dilemma" || guess == "Memory" || guess == "Remember")
+    {
+
+            Console.WriteLine("Incorrect!");
+            guess = Console.ReadLine();
+            score--;
+    }
+    if (guess == "Member")
+    {
+            Console.WriteLine("Correct!");
+
+    }
+}
+    Console.WriteLine($"Your Total Score Is: {score}/60");
