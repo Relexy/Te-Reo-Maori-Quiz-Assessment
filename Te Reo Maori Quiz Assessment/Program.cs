1bb36123a7c95cb747b7f7980fada92f2afad4de
Jason Tan <ac113173@avcol.school.nz>
2022-03-11 19:07:45 +1300

    Add project files.

---
+++ Te Reo Maori Quiz Assessment/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+Console.WriteLine("This Is A Quiz To Increase Or Train Your Knowledge On The Te Reo Maori Language");
+Console.WriteLine("You Are Going To Be Given One Word In Maori And You Are To Type From 4 Words Given That Are Translated In English");
+Console.WriteLine("If You Guess The Right Translated Word You Are Given Points \nIf You Guess Incorrectly Your Chances Of Earning More Points Is Reduced\n\n");
+
+Console.WriteLine("To Start The Quiz Type: 'start' ");
+string start = Console.ReadLine();
+
+while (start != "start")
+{
+    Console.WriteLine("Please Type 'start' To Begin The Quiz.");
+    start = Console.ReadLine();
+}
+
