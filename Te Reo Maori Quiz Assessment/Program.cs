89cfc321e2a91f00997d98e1563773c10a2585c1
Jason Tan <ac113173@avcol.school.nz>
2022-03-13 13:24:37 +1300

    Added Other Quiz Levels, Arrays for each level

--- Te Reo Maori Quiz Assessment/Program.cs
+++ Te Reo Maori Quiz Assessment/Program.cs
@@ -8,6 +8,20 @@ int quizNum = 0;
 string[] words = new string[] { "mema", "atikara", "tutuki", "mutunga", "whakatakere", "haumi", "wairehu", "pakihere", "whare herehere", "pipihi", "ikarangi", "kiato", "huanui", "puaroa", "keteparaha" };
 string[] answers = new string[] { "Member", "Article", "Achieve", "Conclusion", "Bottom", "Ally", "Spray", "Carry", "Jail", "Shoot", "Galaxy", "Compact", "Highway", "Sacred", "Tool" };
 string[] optionsQ1 = new string[] { "dilemma", "memory", "remember"};
+string[] optionsQ2 = new string[] { "attic", "attend", "athletic" };
+string[] optionsQ3 = new string[] { "agile", "trolley", "trust" };
+string[] optionsQ4 = new string[] { "measure", "mountain", "mechanic" };
+string[] optionsQ5 = new string[] { "withdraw", "waiter", "builder" };
+string[] optionsQ6 = new string[] { "heat", "helmet", "helping" };
+string[] optionsQ7 = new string[] { "wrestle", "wardrobe", "wine" };
+string[] optionsQ8 = new string[] { "positivity", "contain", "clapping" };
+string[] optionsQ9 = new string[] { "mansion", "construction", "mall" };
+string[] optionsQ10 = new string[] { "peeking", "produce", "liar" };
+string[] optionsQ11 = new string[] { "inclusive", "irony", "feast" };
+string[] optionsQ12 = new string[] { "know", "kidney", "prestige" };
+string[] optionsQ13 = new string[] { "headquarters", "hardship", "harbor" };
+string[] optionsQ14 = new string[] { "sacrifice", "smash", "portal" };
+string[] optionsQ15 = new string[] { "container", "kitchen", "position" };
 
 for (int i = 0; i < optionsQ1.Length; i++)
 {
@@ -16,7 +30,6 @@ for (int i = 0; i < optionsQ1.Length; i++)
 for (int i = 0; i < answers.Length; i++)
 {
     answers[i] = answers[i].ToLower();
-    Console.WriteLine(answers[i]);
 }
 
 Console.WriteLine("This Is A '15 Level' Quiz To Increase Or Train Your Knowledge On The Te Reo Maori Language");
@@ -34,7 +47,7 @@ while (start.ToLower() != "start")
 
 if (start.ToLower() == "start")
 {
-    Console.WriteLine($"Your First Word Is... {words[quizNum]}");
+    Console.WriteLine($"Your 1st Word Is... {words[quizNum]}");
     Console.WriteLine($"A. {optionsQ1[0]} \nB. {optionsQ1[1]} \nC. {optionsQ1[2]} \nD. {answers[0]} \n(Remember To Type The Word)");
  
 
@@ -74,13 +87,73 @@ if (start.ToLower() == "start")
             switch (quizNum)
             {
                 case  1:
-                Console.WriteLine($"Your Second Word Is... {words[quizNum]}");
-                Console.WriteLine($"A. {optionsQ1[0]} \nB. {optionsQ1[1]} \nC. {optionsQ1[2]} \nD. {optionsQ1[3]} \n(Remember To Type The Word)");
+                Console.WriteLine($"Your 2nd Word Is... {words[quizNum]}");
+                Console.WriteLine($"A. {optionsQ2[0]} \nB. {answers[1]} \nC. {optionsQ2[1]} \nD. {optionsQ2[2]} \n(Remember To Type The Word)");
                     break;
 
                 case 2:
-                Console.WriteLine($"Your Second Word Is... {words[quizNum]}");
-                Console.WriteLine($"A. {optionsQ1[0]} \nB. {optionsQ1[1]} \nC. {optionsQ1[2]} \nD. {optionsQ1[3]} \n(Remember To Type The Word)");
+                Console.WriteLine($"Your 3rd Word Is... {words[quizNum]}");
+                Console.WriteLine($"A. {optionsQ3[0]} \nB. {answers[2]} \nC. {optionsQ3[1]} \nD. {optionsQ3[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 3:
+                    Console.WriteLine($"Your 4th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ4[0]} \nB. {answers[3]} \nC. {optionsQ4[1]} \nD. {optionsQ4[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 4:
+                    Console.WriteLine($"Your 5th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ5[0]} \nB. {optionsQ5[1]} \nC. {optionsQ5[2]} \nD. {answers[4]} \n(Remember To Type The Word)");
+                    break;
+
+                case 5:
+                    Console.WriteLine($"Your 6th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ6[0]} \nB. {answers[5]} \nC. {optionsQ6[1]} \nD. {optionsQ6[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 6:
+                    Console.WriteLine($"Your 7th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {answers[6]} \nB. {optionsQ7[0]} \nC. {optionsQ7[1]} \nD. {optionsQ7[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 7:
+                    Console.WriteLine($"Your 8th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ8[0]} \nB. {answers[7]} \nC. {optionsQ8[1]} \nD. {optionsQ8[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 8:
+                    Console.WriteLine($"Your 9th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ9[0]} \nB. {optionsQ9[1]} \nC. {optionsQ9[2]} \nD. {answers[8]} \n(Remember To Type The Word)");
+                    break;
+
+                case 9:
+                    Console.WriteLine($"Your 10th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {answers[9]} \nB. {optionsQ10[0]} \nC. {optionsQ10[1]} \nD. {optionsQ10[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 10:
+                    Console.WriteLine($"Your 11th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ11[0]} \nB. {answers[10]} \nC. {optionsQ11[1]} \nD. {optionsQ11[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 11:
+                    Console.WriteLine($"Your 12th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ12[0]} \nB. {optionsQ12[1]} \nC. {answers[11]} \nD. {optionsQ12[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 12:
+                    Console.WriteLine($"Your 13th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {answers[12]} \nB. {optionsQ13[0]} \nC. {optionsQ13[1]} \nD. {optionsQ13[2]} \n(Remember To Type The Word)");
+                    break;
+
+                case 13:
+                    Console.WriteLine($"Your 14th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ14[0]} \nB. {optionsQ14[1]} \nC. {optionsQ14[2]} \nD. {answers[13]} \n(Remember To Type The Word)");
+                    break;
+
+                case 14:
+                    Console.WriteLine($"Your 15th Word Is... {words[quizNum]}");
+                    Console.WriteLine($"A. {optionsQ15[0]} \nB. {answers[14]} \nC. {optionsQ15[1]} \nD. {optionsQ15[2]} \n(Remember To Type The Word)");
                     break;
             }
         }
